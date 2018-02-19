821c5c102de5cc4b3acfc6ee8a4ab14432dfd68e
mckendu <duncan.mckenna@aerlingus.com>
2018-02-19 09:46:22 +0000

    fixed display consistency for news sections

--- wp-content/themes/vdc_DEV/page-managing-volunteers.php
+++ wp-content/themes/vdc_DEV/page-managing-volunteers.php
@@ -18,42 +18,14 @@
 		$wp_query = new WP_Query( $args ); 
 		$count = $wp_query->post_count;
 	?>
-
-
 	<?php if ( $wp_query->have_posts() ) : ?>
 	<div class="container vdc-blog-overview">
 		<h1 class="text-center">Managing Volunteers</h1>
 		<hr style="max-width: 80%;">
-		<div class="row">
-			<?php $i = 1; while ( $wp_query->have_posts() ) : $wp_query->the_post();?>	
-				<div class="col-md-6">
-					<a href="<?php the_permalink(); ?>" <?php post_class(); ?>>
-
-						<h2><?php the_title(); ?></h2>
-
-						<?php if (has_post_thumbnail()) : ?>
-							<div class="featured-image">
-								<?php the_post_thumbnail(); ?>
-							</div>
-						<?php endif; ?>
-
-						<div class="entry-content index-excerpt">
-							<?php
-								echo wbstarter_excerpt('26');
-							?>
-						</div>
-					</a>
-				</div>
-				<?php if ($i % 2 == 0): ?>
-					<div class="clearfix visible-sm-block visible-md-block visible-lg-block"></div>
-					<?php if ($i < $count): ?>
-						<hr>
-					<?php endif ?>
-					
-				<?php endif; ?>		
-
-			<?php $i++; endwhile; ?>
-			<?php wp_reset_postdata(); ?>
+		
+		<?php get_template_part( 'template-parts/post-roll', 'none' ); ?>
+		
+		<?php wp_reset_postdata(); ?>
 		
 	</div>
 	<div class="container">
